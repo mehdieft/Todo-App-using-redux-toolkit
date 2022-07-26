499ff4811655594cb798880c55831918b79dff58
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-26 23:38:09 +0430

    adding custom font

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import TodoHeader from './src/components/TodoHeader';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+
+      <TodoHeader/>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,1 +1,29 @@
-eggergerger
+import{View,Text,StyleSheet} from 'react-native';
+import {useFonts} from 'expo-font'
+import { AlexBrush_400Regular } from '@expo-google-fonts/alex-brush'
+import AppLoading from 'expo-app-loading';
+
+export default function TodoHeader(){
+    let [fontLoaded,error]=useFonts({
+        alex:AlexBrush_400Regular
+    })
+    if(!fontLoaded){
+        return <AppLoading/>
+    }
+    return (
+        <View>
+            <Text style={styles.text}>this is header</Text>
+        </View>
+
+    )
+}
+const styles=StyleSheet.create({
+    text:{
+        fontSize:20,
+        fontWeight:'bold',
+        textAlign:'center',
+        marginTop:20,
+        fontFamily:'alex'
+    }
+
+})
