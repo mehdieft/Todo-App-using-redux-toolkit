d95f76ee4d2e94e4d9d185122482ea35fe934bb1
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-27 01:25:34 +0430

    system hanged

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
-import TodoHeader from './src/components/TodoHeader'
+import TodoHeader from './src/components/TodoHeader';
+import TodoList from './src/components/TodoList';
 export default function App() {
   
   return (
-      <SafeAreaView>
+      <ScrollView style={{marginBottom:300}}>
         <TodoHeader/>
-      </SafeAreaView>
+        <TodoList/>
+      </ScrollView>
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
-import {View,Text,FlatList} from 'react-native';
+import {View,Text,FlatList,StyleSheet} from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
 
 export default function TodoList(){
     const data=[
@@ -6,12 +7,11 @@ export default function TodoList(){
         {id:2,title:'this is simple test'},
         {id:3,title:'this is simple test'},
     ]
-    const renderItem=(item)=>{
+    const renderItem=({item})=>{
         return (
             <View style={styles.todoContainer}>
-                <Text style={styles.todoText}></Text>
-                <Text></Text>
-
+                <Text style={styles.todoText}>{item.title}</Text>
+                <AntDesign name="delete" size={24} color="black" />
             </View>
         )
     }
@@ -22,6 +22,7 @@ export default function TodoList(){
             style={styles.todos}
             data={data}
             keyExtractor={(item)=>item.id.toString()} 
+            renderItem={renderItem}
 
             />
         </View>
@@ -29,13 +30,18 @@ export default function TodoList(){
 }
 const styles=StyleSheet.create({
     todos:{
-        justifyContent:'center',
-        alignItems: 'center'
+        // justifyContent:'center',
+        // alignItems: 'center'
     },
     todoContainer:{
         backgroundColor:'#321123',
         borderRadius:5,
-        marginTop:12
+        marginTop:12,
+        flexDirection: 'row',
+        textAlign:'center',
+        paddingVertical:250
+
+
 
     },todoText:{
         color:'white',
