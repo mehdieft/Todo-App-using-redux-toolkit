49e56b3c0e36f99283f2034a49bb0d1667425aa1
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-26 23:46:10 +0430

    forget adding fonts through the npm

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,29 +1,32 @@
-import{View,Text,StyleSheet} from 'react-native';
-import {useFonts} from 'expo-font'
-import { AlexBrush_400Regular } from '@expo-google-fonts/alex-brush'
-import AppLoading from 'expo-app-loading';
+import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
 
-export default function TodoHeader(){
-    let [fontLoaded,error]=useFonts({
-        alex:AlexBrush_400Regular
-    })
-    if(!fontLoaded){
-        return <AppLoading/>
+
+const [todo,setTodo]=useState('');
+
+export default function TodoHeader() {
+    
+        return (
+            <View>
+                <Text style={styles.text}>this is header</Text>
+                <TextInput
+                style={styles.input}
+                placeholder="add todo"
+                onChangeText={setTodo}
+                value={todo}
+                />
+            </View>
+
+        )
     }
-    return (
-        <View>
-            <Text style={styles.text}>this is header</Text>
-        </View>
 
-    )
-}
-const styles=StyleSheet.create({
-    text:{
-        fontSize:20,
-        fontWeight:'bold',
-        textAlign:'center',
-        marginTop:20,
-        fontFamily:'alex'
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        fontFamily: 'RobotoCondensed_700Bold_Italic'
     }
 
 })
